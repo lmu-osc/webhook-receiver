07497597105946561b753544a22c1516a71c79d4
Patrick Callahan <patricktcallahan18@gmail.com>
2026-03-30 18:01:07 +0200

    Wire up handler for webhook

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
-	println("Hello, World!")
+	http.HandleFunc("/webhook", webhookHandler)
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("server failed: %v", err)
+	}
 }
